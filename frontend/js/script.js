126fcc4b2cbd6e6a3e7e8b5210ee55419260203b
Tabita <if21b148@technikum-wien.at>
2022-04-26 18:12:36 +0200

    addDate, Liste ausgeben,...

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,9 +1,14 @@
 /**use window on load to wait for DOM elements to load*/
-$(window).on("load", () =>
-    getAppointments()
-);
+$(window).on("load", () => {
+    getAppointments();
+    $("#details").hide();
+    $("#addDate").click(addDateOption);
+   // $("#dates").hide()
+});
+
 
 function getAppointments() {
+
     $.ajax({
         type: "GET",
         url: "../backend/controller/AppointmentController.php",
@@ -24,14 +29,44 @@ function getAppointment($appointmentID) {
         dataType: "json",
         success: function (response) {
             //show detail div, and load appointment details into fields;
+            //hide list?
+            $("#list").hide();
+            $("#details").append("<p><strong>Title:</strong>:  </p><p><strong>Location</strong></p><p><strong>Vote until: </strong></p>" );
+            $("#details").show();
         }
     })
 }
 
 function addItemToList(item) {
-    $("#list").append("<p>" + item.Title + "</p>").addEventListener("click", () => getAppointment(item.AppointmentID))
+    console.log(item);
+    $("#list").append("<p id="+item.AppointmentID+"><p><h5>" + item.Title +  "</h5></p><p>"+item.Location+"</p><p>Vote until: "+item.DueDate+"</p></p>");
+    $("#"+item.AppointmentID).click(()=> getAppointment(item.AppointmentID));
+
 }
 
+//////////////////////////////////////////////
+//when creating a new appointment -> add as many date options as you like
+//appends input from date input fields + button with onclick to remove dateOption from list
+function addDateOption() {
+    console.log("something");
+    $("#dateList").append("<p><button class='btn btn-outline-danger btn-sm' onclick='$(this).parent().remove()'>X</button>Date: "
+        + $("#dateOption").val() + " Start Time: "+$("#startTime").val()+"</p>");
+};
+
+function removeDate(){
+    console.log(this);
+    $(this).remove();
+}
+//TO DO Remove
+
+$("#submitDates").on("click", function(){
+    var listItems = $("#dateList").childNodes;
+    for(child in listItems){
+        console.log(listItems[child]);
+    }
+});
+
+////////////////////////////////////////////
 
 /*
 function submit() {
@@ -54,6 +89,8 @@ function addDate() {
     toggle details onclick
 
 * */
+
+
 // datepicker fügt datum in datelist hinzu
 /// <div id="dateList">
 //      <p>2022-05-01 14:00:00</p>
